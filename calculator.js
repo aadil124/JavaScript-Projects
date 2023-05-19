e3e52e4efe1f6e553bf0fa41e70ff8320c770088
Mohd Adil Ansari <98684212+aadil124@users.noreply.github.com>
2023-05-19 22:44:30 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 function cal(){
- var opr1= document.getElementById("first").value
- var opr2= document.getElementById("second").value
- var ope= document.getElementById("ope").value 
+ let opr1= document.getElementById("first").value
+ let opr2= document.getElementById("second").value
+ let ope= document.getElementById("ope").value 
  
  if (ope == "+") {
      var res= parseInt(opr1)+parseInt(opr2);}
